c395d9a37db7b7dd5826b1341b2fdd9a0a565f26
edward <edwardlfish@gmail.com>
2026-06-10 18:04:35 +0300

    Add dynamic heroId routing in Home and update Main routes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,7 +10,7 @@ const Main = () => {
 
     return (
         <Routes>
-            {[`/`, `/${navItems[0]}`].map(p => <Route key={p} path={p} element={<Home/>}/>)}
+            {[`/`, `/${navItems[0]}`, `/${navItems[0]}/:heroId`].map(p => <Route key={p} path={p} element={<Home/>}/>)}
             {[`/${navItems[1]}`, `/${navItems[1]}/:heroId`].map(p => <Route key={p} path={p} element={<AboutMe/>}/>)}
             <Route path={`/${navItems[2]}`} element={<StarWars/>}/>
             <Route path={`/${navItems[3]}`} element={<Contact/>}/>
